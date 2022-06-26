7bc7dd1bb474fc453bdfa0354b937ded8654346f
Tanasoaia Ana-Maria <tanasoaiaanamaria@gmail.com>
2022-06-26 16:12:48 +0300

    functions for first repetitive steps from each test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,22 @@
-Cypress.Commands.add("LogIN",()=>{
-        
-        cy.get('.btn-nav').contains('Log In').click()
-        cy.get('.btn-login').contains('Login with your existing account').click()
-        cy.get('input[name="email"]').type('user10@test.com')
-        cy.get('input[name="password"]').type('user123456')
-        cy.get('.submit-btn').contains('Log In as a member').click()
+Cypress.Commands.add("Launch_the_website",() => {
+        //inserting the URL
+        cy.visit('https://automationexercise.com/')
+        //verify if the page title is Automation Exercise
+        cy.title().should('eq','Automation Exercise')
+        //verify the protocol of the page
+        cy.location('protocol').should('eq','https:')
+        //verify the title of the page
+        cy.title().should('eq','Automation Exercise')
+})
+
+Cypress.Commands.add("LogIN_is_possible",() => {
+        //Verify that home page is visible successfully
+        cy.get('a > img').should('be.visible');
+        //Click on 'Signup / Login' button
+        cy.get('.shop-menu > .nav > :nth-child(4) > a').should('be.visible').click();
+        //Verify 'New User Signup!' is visible
+        cy.contains('Login to your account').should('be.visible');
+        //the credentials for login can be inserted
+        cy.get('.login-form > form > [type="email"]').should('not.be.disabled');
+        cy.get('.login-form > form > [type="email"]').should('not.be.disabled');
 })
